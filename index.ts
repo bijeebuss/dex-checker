2009447befef8371cce504543f0c2bcb912bc2e3
Michael <mwelnick@icloud.com>
2020-02-09 15:54:20 -0700

    notification

--- index.ts
+++ index.ts
@@ -4,8 +4,18 @@ async function main() {
     const result = await fetch('https://live-api.dex.ag/price?from=TUSD&to=DAI&toAmount=2000&tradable=true&discluded=ddex,&ssr=false')
     const kyber = (await result.json() as any[]).find(d => d.dex === 'kyber')
 
-    if(Number(kyber.price) < 1.008)
-        console.log(kyber.price)
+    if(Number(kyber.price) < 1.01)
+    await fetch(`https://maker.ifttt.com/trigger/price_alert/with/key/${process.env.IFTTT_KEY}`, 
+    {
+        method: 'POST', 
+        body: JSON.stringify({
+            value1: kyber.price
+        }),
+        headers: {'Content-Type': 'application/json'}
+    })
+
 }
 
-main()
+main().catch(err => {
+    throw err
+})
