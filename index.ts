0a4a96e975263e2c29cbc332c945dfa1e724cc0c
Michael <mwelnick@icloud.com>
2020-02-09 14:36:22 -0700

    test

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ async function main() {
     const result = await fetch('https://live-api.dex.ag/price?from=TUSD&to=DAI&toAmount=2000&tradable=true&discluded=ddex,&ssr=false')
     const kyber = (await result.json() as any[]).find(d => d.dex === 'kyber')
 
-    if(Number(kyber.price) < 1.08)
+    if(Number(kyber.price) < 1.008)
         console.log(kyber.price)
 }
 
