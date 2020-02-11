d9f9a20ebe453c90404fab617e468ba4ff89e4bd
Michael <mwelnick@icloud.com>
2020-02-10 17:59:36 -0700

    do USC too

--- index.ts
+++ index.ts
@@ -1,19 +1,26 @@
 import {default as fetch} from 'node-fetch'
 
 async function main() {
-    const result = await fetch('https://live-api.dex.ag/price?from=TUSD&to=DAI&toAmount=2000&tradable=true&discluded=ddex,&ssr=false')
-    const kyber = (await result.json() as any[]).find(d => d.dex === 'kyber')
+    const tokens = [
+        {name: 'USDC', price: 1.005}, 
+        {name: 'TUSD', price: 1.007}
+    ]
 
-    if(Number(kyber.price) < 1.008)
-    await fetch(`https://maker.ifttt.com/trigger/price_alert/with/key/${process.env.IFTTT_KEY}`, 
-    {
-        method: 'POST', 
-        body: JSON.stringify({
-            value1: kyber.price
-        }),
-        headers: {'Content-Type': 'application/json'}
-    })
+    for (const token of tokens) {
+        const result = await fetch(`https://live-api.dex.ag/price?from=${token.name}&to=DAI&toAmount=2000&tradable=true&discluded=ddex,&ssr=false`)
+        const kyber = (await result.json() as any[]).find(d => d.dex === 'kyber')
 
+        if(Number(kyber.price) < token.price)
+        await fetch(`https://maker.ifttt.com/trigger/price_alert/with/key/${process.env.IFTTT_KEY}`, 
+        {
+            method: 'POST', 
+            body: JSON.stringify({
+                value1: kyber.price,
+                value2: token.name
+            }),
+            headers: {'Content-Type': 'application/json'}
+        })
+    }
 }
 
 main().catch(err => {
